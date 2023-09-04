97d1bb4a78e5216ee4fff04ff2599eee02eac50f
Yawar Ejaz <130927411+yawar-ejaz@users.noreply.github.com>
2023-09-05 01:17:26 +0530

    adding data to local storage in object form

--- app.js
+++ app.js
@@ -7,11 +7,17 @@ function addToLocalStorage(e) {
     let name = e.target.name.value;
     let mail = e.target.mail.value;
     let phone = e.target.phone.value;
-    console.log(name);
-    console.log(mail);
-    console.log(phone);
-
-    localStorage.setItem("name", name);
-    localStorage.setItem("mail", mail);
-    localStorage.setItem("phone", phone);
+    
+    e.target.name.value = "";
+    e.target.mail.value = "";
+    e.target.phone.value = "";
+    
+    let person = {
+        name: name,
+        mail: mail,
+        phone: phone
+    };
+    
+    person_serialized = JSON.stringify(person);
+    localStorage.setItem("Object 1", person_serialized);
 }
