5b6c85e4ecc4432480de8b00de4ae8daa1a6d7f9
Yawar Ejaz <130927411+yawar-ejaz@users.noreply.github.com>
2023-09-05 12:32:16 +0530

    Added deleting functionality

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 let form = document.getElementById("form");
+let blank_ul = document.getElementById("blank-ul");
+
+blank_ul.addEventListener("click", removeItem);
+function removeItem(e) {
+    if (e.target.classList.contains("delete")) {
+        let li = e.target.parentElement;
+        blank_ul.removeChild(li);
+    }
+}
 
 form.addEventListener("submit", addToLocalStorage);
 
@@ -19,8 +28,35 @@ function addToLocalStorage(e) {
     };
     
     person_serialized = JSON.stringify(person);
-
-    let blank_space = document.getElementById("blank-space");
-    blank_space.innerHTML = person_serialized;
     localStorage.setItem(mail, person_serialized);
+
+    let del_btn = document.createElement("button");
+    del_btn.className = "btn btn-danger btn-sm float-right delete";
+    del_btn.appendChild(document.createTextNode("Delete"));
+    let new_li = document.createElement("li");
+    new_li.className = "list-group-item";
+    let text = document.createTextNode(person_serialized);
+    new_li.appendChild(text);
+    new_li.appendChild(del_btn);
+    blank_ul.appendChild(new_li);
 }
+
+function printList() {
+    let l = localStorage.length;
+    for (let i = 0; i < l; i++){
+        let k = localStorage.key(i);
+        let val = localStorage.getItem(k);
+
+        let del_btn = document.createElement("button");
+        del_btn.className = "btn btn-danger btn-sm float-right delete";
+        del_btn.appendChild(document.createTextNode("Delete"));
+        let new_li = document.createElement("li");
+        new_li.className = "list-group-item";
+        let text = document.createTextNode(val);
+        new_li.appendChild(text);
+        new_li.appendChild(del_btn);
+        blank_ul.appendChild(new_li);
+    }
+}
+
+printList();
