5b08b60dcbbbbf1aed55aa3afb3ac7beb1b5e238
Yawar Ejaz <130927411+yawar-ejaz@users.noreply.github.com>
2023-09-05 14:37:14 +0530

    Now you can edit or delete items

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 let form = document.getElementById("form");
 let blank_ul = document.getElementById("blank-ul");
 
-blank_ul.addEventListener("click", removeItem);
-function removeItem(e) {
+blank_ul.addEventListener("click", removeOrEditItem);
+function removeOrEditItem(e) {
     if (e.target.classList.contains("delete")) {
         let li = e.target.parentElement;
         const text = li.firstChild.textContent;
 
         person_deserialized = JSON.parse(text);
-        console.log(person_deserialized);
         let key = person_deserialized.mail;
 
-        console.log(key);
         blank_ul.removeChild(li);
         localStorage.removeItem(key);
+    }
+
+    if (e.target.classList.contains("edit")) {
+        let li = e.target.parentElement;
+        const text = li.firstChild.textContent;
+
+        person_deserialized = JSON.parse(text);
+        let key = person_deserialized.mail;
+        let name = person_deserialized.name;
+        let phone = person_deserialized.phone;
+
+
+        blank_ul.removeChild(li);
+        localStorage.removeItem(key);
+        document.getElementById("name").value = name;
+        document.getElementById("mail").value = key;
+        document.getElementById("phone").value = phone;
     }
 }
 
@@ -41,11 +56,15 @@ function addToLocalStorage(e) {
     let del_btn = document.createElement("button");
     del_btn.className = "btn btn-danger btn-sm float-right delete";
     del_btn.appendChild(document.createTextNode("Delete"));
+    let edit_btn = document.createElement("button");
+    edit_btn.className = "btn btn-primary btn-sm float-right mr-2 edit";
+    edit_btn.appendChild(document.createTextNode("Edit"));
     let new_li = document.createElement("li");
     new_li.className = "list-group-item";
     let text = document.createTextNode(person_serialized);
     new_li.appendChild(text);
     new_li.appendChild(del_btn);
+    new_li.appendChild(edit_btn);
     blank_ul.appendChild(new_li);
 }
 
@@ -58,11 +77,17 @@ function printList() {
         let del_btn = document.createElement("button");
         del_btn.className = "btn btn-danger btn-sm float-right delete";
         del_btn.appendChild(document.createTextNode("Delete"));
+
+        let edit_btn = document.createElement("button");
+        edit_btn.className = "btn btn-primary btn-sm float-right mr-2 edit";
+        edit_btn.appendChild(document.createTextNode("Edit"));
+        
         let new_li = document.createElement("li");
         new_li.className = "list-group-item";
         let text = document.createTextNode(val);
         new_li.appendChild(text);
         new_li.appendChild(del_btn);
+        new_li.appendChild(edit_btn);
         blank_ul.appendChild(new_li);
     }
 }
